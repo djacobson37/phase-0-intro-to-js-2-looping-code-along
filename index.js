7a3f6946a24c53d33001f11000e303aeef3ae86a
David F. Jacobson <jakelis2018@gmail.com>
2022-11-07 12:12:02 -0600

    commit tree f3e5ecf65236bda7ba039129c5c5b044edec7fec

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const writeCards = (arr,event) => {
+  const messages = []
+  for(let i = 0; i < arr.length;i++) {
+      
+         const greetingMessage = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+         messages.push(greetingMessage)
+         
+  }
+ return messages
+
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"))
+
+var countdown = 11;
+while (countdown > 0){
+    countdown--;
+    console.log(countdown);
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -14,8 +14,8 @@ describe( 'index.js', () => {
   describe( 'writeCards()', () => {
 
     it( 'returns an array of thank you messages for each name provided to the function', () => {
-      expect(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")).to.deep.eq([
-        "Thank you, Guadalupe, for the wonderful surprise gift!",
+      
+        "Thank you, Guadalupe, for the wonderful surprise gift!",expect(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")).to.deep.eq([
         "Thank you, Ollie, for the wonderful surprise gift!",
         "Thank you, Aki, for the wonderful surprise gift!",
       ]);
